ac97e9f6cddbc9a8f5b6d1eb9db7fa224b416a0f
Mu Yuan <MYCOJER@GMAIL.COM>
2021-12-21 00:04:27 +0800

    UserType CRUD

--- src/main/java/com/team7/leave/services/UserTypeService.java
+++ src/main/java/com/team7/leave/services/UserTypeService.java
@@ -1,5 +1,20 @@
 package com.team7.leave.services;
 
+import java.util.ArrayList;
+
+import javax.validation.Valid;
+
+import com.team7.leave.model.UserType;
+
 public interface UserTypeService {
 
+	UserType createUserType(UserType userType);
+
+	ArrayList<UserType> findAllUserType();
+
+	UserType findUserType(Integer id);
+
+	UserType updateUserType(UserType userType);
+
+	void delUserType(Integer id);
 }
