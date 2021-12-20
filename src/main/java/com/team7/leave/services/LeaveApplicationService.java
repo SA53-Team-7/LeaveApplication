3e45fb4c57b16398b6b400dfa9a80c2f46350718
Ashira Wee <weeashira@gmail.com>
2021-12-21 05:44:00 +0800

    Commit Create (excluding validation, logic to check PH) and View Leave History

--- src/main/java/com/team7/leave/services/LeaveApplicationService.java
+++ src/main/java/com/team7/leave/services/LeaveApplicationService.java
@@ -1,5 +1,18 @@
 package com.team7.leave.services;
 
-public interface LeaveApplicationService {
+import java.util.List;
 
+import com.team7.leave.model.LeaveApplication;
+import com.team7.leave.model.LeaveType;
+
+public interface LeaveApplicationService {
+	List<LeaveType> findAllLeaveType();
+	
+	LeaveApplication createLeaveApplication(LeaveApplication leaveapp);
+	
+	List<LeaveApplication> viewAllLeaveApplications(Integer employeeId, Integer year);
+	
+	LeaveApplication updateLeaveApplication(LeaveApplication leaveapp);
+	
+	void deleteLeaveApplication(LeaveApplication leaveapp);
 }
--- src/main/java/com/team7/leave/services/LeaveApplicationServiceImpl.java
+++ src/main/java/com/team7/leave/services/LeaveApplicationServiceImpl.java
@@ -1,8 +1,51 @@
 package com.team7.leave.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.team7.leave.Repositories.LeaveApplicationRepository;
+import com.team7.leave.Repositories.LeaveTypeRepository;
+import com.team7.leave.model.LeaveApplication;
+import com.team7.leave.model.LeaveType;
 
 @Service
 public class LeaveApplicationServiceImpl implements LeaveApplicationService {
+	@Autowired
+	LeaveTypeRepository ltrepo;
+	
+	@Autowired
+	LeaveApplicationRepository larepo;
+		
+	@Override
+	public List<LeaveType> findAllLeaveType() {
+		List<LeaveType> leavetypes = ltrepo.findAll();
+		return leavetypes;
+	}
 
+	@Override
+	@Transactional
+	public LeaveApplication createLeaveApplication(LeaveApplication leaveapp) {
+		return larepo.saveAndFlush(leaveapp);
+	}
+	
+	// Gets all applications from the current year
+	@Override
+	public List<LeaveApplication> viewAllLeaveApplications(Integer employeeId, Integer year) {
+		return larepo.findAllLeaveAppByEmpId(employeeId, year);
+	}
+
+	@Override
+	public LeaveApplication updateLeaveApplication(LeaveApplication leaveapp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteLeaveApplication(LeaveApplication leaveapp) {
+		// TODO Auto-generated method stub
+		
+	}
 }
