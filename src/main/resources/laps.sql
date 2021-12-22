3987324b2e69f964644d10c5ca1a3254f4de2d35
Lina <82659876+Wanderstarz@users.noreply.github.com>
2021-12-22 15:42:55 +0800

    Merge remote-tracking branch 'origin/main' into Lina2

--- src/main/resources/laps.sql
+++ src/main/resources/laps.sql
@@ -5,18 +5,6 @@ VALUES
 (2,"staff",18),
 (3,"manager",18);
 
-INSERT INTO laps.employee
-VALUES
-(1, "first@gmail.com", 10, 5, "pupu", "bob", 20, "bob", "bob", 1)
-
-INSERT INTO laps.employee
-VALUES
-(2, "second@gmail.com", 20, 23, null, "pupu", 20, "pupu", "pupu", 3)
-
-INSERT INTO laps.employee
-VALUES
-(3, "third@gmail.com", 20, 23, "pupu", "tony", 20, "tony", "tony", 2)
-
 INSERT INTO laps.public_holiday
 (public_holiday.date_from, public_holiday.name)
 VALUES
@@ -31,3 +19,21 @@ VALUES
 ("2021-08-09", "National Day"),
 ("2021-11-04", "Deepavali"),
 ("2021-12-25", "Christmas Day");
+
+INSERT INTO laps.leave_type
+(leave_type_id, description, type)
+VALUES
+(1,"An employee has to take full day leave","Annual Leave"),
+(2,"An employee has to take full day leave","Medical Leave"),
+(3,"For compensation leave the granularity is half a day. Every four hours of overtime work makes an employee eligible for half-a day compensation.","Compensation Leave");
+
+INSERT INTO laps.leave_application
+(leave_id, date_from, date_to, memo, reason, status, employee_employeeid, leavetype_leave_type_id)
+VALUES
+(1, '2021-12-10', '2021-12-22', "nil", "Personal", "APPLIED", 1, 1 ),
+(2, '2021-02-22', '2021-04-05', "nil", "Vacation", "APPROVED", 1, 1 ),
+(3, '2021-07-21', '2021-08-22', "nil", "Overseas", "REJECTED", 3, 1 ),
+(4, '2021-08-22', '2021-08-25', "nil", "Personal", "APPLIED", 3, 1 ),
+(5, '2021-12-18', '2021-12-22', "nil", "Personal", "APPLIED", 3, 1 ),
+(6, '2021-10-22', '2021-10-26', "nil", "Vacation", "APPLIED", 2, 1 );
+
