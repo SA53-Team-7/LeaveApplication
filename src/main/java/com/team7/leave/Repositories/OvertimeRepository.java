9955ba3fdc92a54e5ba47d13881453dbb75efeeb
Oliver Tan <61538053+ohanyang@users.noreply.github.com>
2021-12-23 22:59:36 +0800

    Merge branch 'Oliver'
    # Conflicts:
    #	src/main/java/com/team7/leave/model/Employee.java
    #	src/main/resources/application.properties
    #	src/main/resources/messages.properties

--- src/main/java/com/team7/leave/Repositories/OvertimeRepository.java
+++ src/main/java/com/team7/leave/Repositories/OvertimeRepository.java
@@ -9,10 +9,10 @@ import com.team7.leave.model.Overtime;
 
 public interface OvertimeRepository extends JpaRepository<Overtime, Integer> {
 
-	@Query(value = "Select * from overtime where employee_employeeid = :employeeId", nativeQuery=true)
+	@Query(value = "Select * from overtime where employee_employee_id = :employeeId", nativeQuery=true)
 	List<Overtime> getAllOTClaimsList (Integer employeeId);
 	
-	@Query(value = "Select * from overtime inner join employee on overtime.employee_employeeid = employee.employeeid where employee.managedby = :managerUsername and overtime.status = 'APPLIED'", nativeQuery=true)
+	@Query(value = "Select * from overtime inner join employee on overtime.employee_employee_id = employee.employee_id where employee.managedby = :managerUsername and overtime.status = 'APPLIED'", nativeQuery=true)
 	List<Overtime> getListForApproval (String managerUsername);
 	
 	@Query(value = "Select * from overtime where overtime_id = :id", nativeQuery=true)
