5b22564cb3d95d6f1dec89ab874ae518bee6b7af
frozenpotatoes <52646745+si-thu-zaw@users.noreply.github.com>
2021-12-23 18:48:00 +0800

    Minor updates to Manager Leave List

--- src/main/resources/laps.sql
+++ src/main/resources/laps.sql
@@ -5,26 +5,20 @@ VALUES
 (2,"staff",18),
 (3,"manager",18);
 
-<<<<<<< HEAD
-=======
--- for login page ↓
-
-
 INSERT INTO laps.employee
 VALUES
-(1, "first@gmail.com", 10, 5, "pupu", "bob", 20, "bob", "bob", 1)
-(2, "second@gmail.com", 20, 23, null, "pupu", 20, "pupu", "pupu", 3)
-(3, "third@gmail.com", 20, 23, "pupu", "tony", 20, "tony", "tony", 2)
+(1, "first@gmail.com", 10, 5, "pupu", "bob", 20, "bob", "bob", 1),
+(2, "second@gmail.com", 20, 23, null, "pupu", 20, "pupu", "pupu", 3),
+(3, "third@gmail.com", 20, 23, "pupu", "tony", 20, "tony", "tony", 2);
 
--- for login page
 
 INSERT INTO laps.employee
 VALUES
 (1, "first@gmail.com", 10, 5, "pupu", "bob", 20, "bob", "bob", 1),
 (2, "second@gmail.com", 20, 23, null, "pupu", 20, "pupu", "pupu", 3),
-(3, "third@gmail.com", 20, 23, "pupu", "tony", 20, "tony", "tony", 2)
+(3, "third@gmail.com", 20, 23, "pupu", "tony", 20, "tony", "tony", 2);
 
->>>>>>> refs/remotes/origin/main
+
 INSERT INTO laps.public_holiday
 (public_holiday.date_from, public_holiday.name)
 VALUES
@@ -40,7 +34,6 @@ VALUES
 ("2021-11-04", "Deepavali"),
 ("2021-12-25", "Christmas Day");
 
-<<<<<<< HEAD
 INSERT INTO laps.leave_type
 (leave_type_id, description, type)
 VALUES
@@ -58,12 +51,14 @@ VALUES
 (5, '2021-12-18', '2021-12-22', "nil", "Personal", "APPLIED", 3, 1 ),
 (6, '2021-10-22', '2021-10-26', "nil", "Vacation", "APPLIED", 2, 1 );
 
-=======
--- for manage leave type ↓
-
 INSERT INTO leave_type
 VALUES
 (1, "for annual leave", "annual leave"),
 (2, "for medical leave", "medical leave"),
-(3, "for compensation leave", "compensation leave")
->>>>>>> refs/remotes/origin/main
+(3, "for compensation leave", "compensation leave");
+
+INSERT INTO `leave_application` (`leave_id`, `date_from`, `date_to`, `manager_comments`, `memo`, `reason`, `status`, `employee_employee_id`, `leavetype_leave_type_id`) VALUES
+(1, '1998-12-31', '1998-12-31', '', 'test', 'test', 'APPLIED', 3, 1),
+(2, '1998-12-31', '1998-12-31', '', 'ttest', 'tes', 'APPROVED', 3, 1),
+(3, '2021-12-01', '2021-12-02', NULL, 'test', 'test', 'APPLIED', 3, 1),
+(4, '2021-12-01', '2021-12-02', '', 'test', 'test', 'REJECTED', 3, 1);
